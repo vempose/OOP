11b7e57496916220eb0cba0550a23543c7e62f70
Alexander C. <vempose@gmail.som>
2026-02-11 01:25:01 +0500

    lab1: solve problem1

---
+++ src/Lab1/Problem1.java
@@ -0,0 +1,73 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    static void main() {
+        Analyzer analyzer = new Analyzer();
+        analyzer.run();
+    }
+}
+
+class Data {
+    private double sum;
+    private double largest;
+    private int count;
+
+    public Data() {
+        this.sum = 0;
+        this.count = 0;
+        this.largest = 0;
+    }
+
+    public void addValue(double value) {
+        if (count == 0) {
+            largest = value;
+        } else {
+            if (value > largest) {
+                largest = value;
+            }
+        }
+        sum += value;
+        count++;
+    }
+
+    public double getAverage() {
+        if (count == 0) {
+            return 0;
+        }
+        return sum / count;
+    }
+
+    public double getLargest() {
+        return largest;
+    }
+}
+
+class Analyzer {
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        Data data = new Data();
+
+        while (true) {
+            System.out.print("Enter number (Q to quit): ");
+            String input = scanner.next();
+
+            if (input.equalsIgnoreCase("Q")) {
+                break;
+            }
+
+            try {
+                double value = Double.parseDouble(input);
+                data.addValue(value);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        System.out.println("Average = " + data.getAverage());
+        System.out.println("Maximum = " + data.getLargest());
+
+        scanner.close();
+    }
+}
